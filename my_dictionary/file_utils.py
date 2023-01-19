49e9c3271937a973f772909a9693a1942cdbc1ed
LuckyBirdFlying <54919057+luckyJim-dev@users.noreply.github.com>
2023-01-19 22:44:46 +0800

    create a communication protocol dictionary

--- my_dictionary/file_utils.py
+++ my_dictionary/file_utils.py
@@ -1,20 +1,25 @@
 
 class MdxItem:
-    def __init__(self, key='', body=''):
+    def __init__(self, key='', body='', img_src='', author='249850395@qq.com'):
         self.key = key
+        self.body_key = '<font size=5>{0}</font>'.format(self.key)
         self.body = body
+        self.author='<br>\r\nupdated by {0}\r\n'.format(author)
+        # self.img_src = '<img src="{0}}">'.format(img_src)
     def to_mdx_txt(self):
-        body_key ='<font size=5>{0}</font>'.format(self.key)
-        mdx_txt = '{0}\r\n{1}\r\n<br>\r\n{2}</>'.format(self.key, body_key, self.body)
+        mdx_txt = ''
+        if not self.key:
+            return mdx_txt
+        mdx_txt = '{0}\r\n{1}\r\n<br>\r\n{2}{3}</>'.format(self.key, self.body_key, self.body, self.author)
         return mdx_txt
-
 
 def csv_to_mdx_txt(path_csv, path_txt):
     mdx_items = []
     with open(path_csv, encoding='utf-8') as f:
         for line in f.readlines():
             line.strip()
-            [key,body] = line.split(',')
+            print(line)
+            [key,body] = line.split(':')
             mdx_item = MdxItem(key,body)
             mdx_item_txt = mdx_item.to_mdx_txt()
             mdx_items.append(mdx_item_txt)
@@ -24,6 +29,6 @@ def csv_to_mdx_txt(path_csv, path_txt):
         f.write(mdx_items_str)
 
 if __name__ == '__main__':
-    path_csv = 'Mydictionary/example.csv'
-    path_txt = 'Mydictionary/example.txt'
+    path_csv = 'my_dictionary/example_file.txt'
+    path_txt = 'my_dictionary/example.txt'
     csv_to_mdx_txt(path_csv, path_txt)
